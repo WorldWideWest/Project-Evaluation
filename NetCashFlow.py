1e430b6c433e452669bea44cc5e11d6477ad2af0
WorldWideWest <dzeno.dzafic@windowslive.com>
2020-11-11 21:21:18 +0100

    Finishing the DataFrame
    Adding values to the dataFrame that will contain all the values

--- NetCashFlow.py
+++ NetCashFlow.py
@@ -25,4 +25,52 @@ class NCFTable():
         print(f"Average Net Cash Flow for this project: {AvgNetCashFlow}")
         print(f"Average Yearly Profit Rate for this project: {AvgYrProfit}%")
 
+
+
+        statistics = pd.DataFrame(
+            [
+                ['Net Cash Flow', NCF],
+                ['Net Value', NetValue],
+                ['Average Net Cash Flow', AvgNetCashFlow],
+                ['Average Yearly Profit Rate', AvgYrProfit]
+            ],
+            columns = ['Description', 'Project']
+        ) 
+        
+        
+        
+        
+        
+
+        dataFrame = pd.DataFrame(columns = ['Year', 'Description', 'Project'])
+
+
+
+
+
+
+
+        ## Adding the investment 
+        for i in range(len(self.investing)):
+            if dataFrame.empty:
+                dataFrame = dataFrame.append(
+                    {'Year': i, 'Description': 'Investment', 'Project': self.investing[i]}, ignore_index=True)
+            else:
+                pass
+        
+        ## Adding the return's
+        if not dataFrame.empty:
+
+            for j in range(0, len(self.returning)):
+                dataFrame = dataFrame.append(
+                    {'Year': j+1, 'Description': 'Return', 'Project': self.returning[j]}, ignore_index=True)
+
+        ## Adding the table summary        
+
+        dataFrame = dataFrame.append(statistics, ignore_index=True)
+                
+                
+        print(dataFrame)
+
         return [NCF, NetValue, AvgNetCashFlow, AvgYrProfit]
+
