35ea8e9816ebc4e1183b99643afd0feff86624fa
WorldWideWest <dzeno.dzafic@windowslive.com>
2020-11-11 11:32:53 +0100

    Spreading the services across multiple files

---
+++ NetCashFlow.py
@@ -0,0 +1,26 @@
+## Table for getting first insights into the investment project
+# IMPORTS
+
+import numpy as np
+import pandas as pd
+
+# END IMPORTS
+
+
+class NCFTable():
+    def __init__(self, investing, returning):
+        self.investing = investing
+        self.returning = returning
+
+    def Table(self):
+        NCF = 0
+        NCF = np.sum(self.returning)
+        NetValue = NCF - self.investing[0]
+        AvgNetCashFlow = NCF / len(self.returning)
+        AvgYrProfit = (AvgNetCashFlow / self.investing[0]) * 100
+        AvgYrProfit = np.round(AvgYrProfit, 3)
+        
+        print(f"Net Cash Flow for this project: {NCF}")
+        print(f"Net Value for this project: {NetValue}")
+        print(f"Average Net Cash Flow for this project: {AvgNetCashFlow}")
+        print(f"Average Yearly Profit Rate for this project: {AvgYrProfit}%")
---
+++ periodChecking.py
@@ -0,0 +1,34 @@
+## From the aspect of investment and income, investment project's devide into group's:
+## 1. Single Investment Cycle - Single Return Cycle
+## 2. Multiple Investment Cycle's - Single Return Cycle
+## 3. Single Investment Cycle - Multiple Return Cycle's
+## 4.  Multiple Investment Cycle's - Multiple Return Cycle's
+
+class InvestmentReturnType():
+    def __init__(self, investmentCycle, returnCycle):
+        self.investmentCycle = investmentCycle
+        self.returnCycle = returnCycle
+
+
+    def CheckType(self):
+        if len(self.investmentCycle) == 1 and len(self.returnCycle) == 1:
+            print("Single Investment Cycle - Single Return Cycle")
+            IRType = [0, 0]
+            return IRType
+        elif len(self.investmentCycle) >= 1 and len(self.returnCycle) == 1:
+            print("Multiple Investment Cycle's - Single Return Cycle")
+            IRType = [1, 0]
+            return IRType
+        elif len(self.investmentCycle) == 1 and len(self.returnCycle) >= 1:
+            print("Single Investment Cycle - Multiple Return Cycle's")
+            IRType = [0, 1]
+            return IRType
+        elif len(self.investmentCycle) >= 1 and len(self.returnCycle) >= 1:
+            print("Multiple Investment Cycle's - Multiple Return Cycle's")
+            IRType = [1, 1]
+            return IRType
+
+
+
+
+
